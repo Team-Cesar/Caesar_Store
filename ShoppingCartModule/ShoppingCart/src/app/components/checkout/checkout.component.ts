18f8ca76e718b39f9c108ce58fe57bb2d5f938b3
Daniel Isaac <trebolman@gmail.com>
2019-01-21 17:29:43 -0500

    Avance Tercer modulo

--- ShoppingCartModule/ShoppingCart/src/app/components/checkout/checkout.component.ts
+++ ShoppingCartModule/ShoppingCart/src/app/components/checkout/checkout.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from 'src/app/services/data.service';
+import { Checkout } from 'src/app/models/Checkout';
 
 
 @Component({
@@ -8,6 +9,8 @@ import { DataService } from 'src/app/services/data.service';
   styleUrls: ['./checkout.component.sass']
 })
 export class CheckoutComponent implements OnInit {
+  public credential:Checkout = new Checkout();
+
   public mensaje:string;
   constructor(private _data:DataService) { }
 
