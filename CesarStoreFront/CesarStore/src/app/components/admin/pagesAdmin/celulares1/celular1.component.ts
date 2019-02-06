48913b60a94c7b58c958d96caa824855056dc60c
Cesar O <omar18pe@gmail.com>
2019-02-05 21:29:00 -0500

    actualizacion

--- CesarStoreFront/CesarStore/src/app/components/admin/pagesAdmin/celulares1/celular1.component.ts
+++ CesarStoreFront/CesarStore/src/app/components/admin/pagesAdmin/celulares1/celular1.component.ts
@@ -1,44 +1,3 @@
-/* import { Component, OnInit } from '@angular/core';
-import { Celular } from '../../interface/celular.interface';
-import { CelularesService } from '../../services/celulares.service';
-
-@Component({
-  selector: 'app-celular1',
-  templateUrl: './celular1.component.html',
-  styles: []
-})
-export class Celular1Component implements OnInit {
-
-  celulares: any;
-  loading: boolean = true;
-
-  constructor( private _celularesService: CelularesService) {
-
-    this._celularesService.getCelulares()
-    .subscribe( data => {
-     this.celulares = data;
-     this.loading = false;
-     console.log(data);
-    });
-   }
-
-  ngOnInit() {
-
-  }
-  borrarCelular(pro_id) {
-    this._celularesService.borrarCelular(pro_id)
-    .subscribe( respuesta => {
-      if ( respuesta) {
-        console.error(respuesta);
-      } else {
-        // todo bien
-        delete this.celulares[pro_id];
-      }
-    });
-  }
-
-}
- */
 import { Component, OnInit } from '@angular/core';
 import { Celular } from '../../interface/celular.interface';
 import { CelularesService } from '../../services/celulares.service';
@@ -50,7 +9,7 @@ import { CelularesService } from '../../services/celulares.service';
 })
 export class Celular1Component implements OnInit {
 
-  celulares: any = [];
+  celulares: Celular;
   loading: boolean = true;
 
   constructor( private _celularesService: CelularesService) {
