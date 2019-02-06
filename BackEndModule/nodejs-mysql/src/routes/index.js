b03952e2d35c67f3c93fcca243dcad3bc3b94402
Cesar O <omar18pe@gmail.com>
2019-02-06 03:20:07 -0500

    pequeña edicion al back|
    edicion en el res para que envie un array

--- BackEndModule/nodejs-mysql/src/routes/index.js
+++ BackEndModule/nodejs-mysql/src/routes/index.js
@@ -6,155 +6,155 @@ const { isLoggedIn } = require('../lib/auth');
 
 // categories
 
-router.get('/findcat/:id', async (req, res)=>{
+router.get('/findcat/:id', async(req, res) => {
     const { id } = req.params;
     const category = await pool.query('SELECT * FROM categories WHERE cat_id = ?', [id]);
-    res.send({category});
+    res.send(category);
 });
 
-router.get('/findallcat', async (req,res)=>{
+router.get('/findallcat', async(req, res) => {
     const categories = await pool.query('SELECT * FROM categories');
-    res.send({categories});
+    res.send(categories);
 });
 
-router.post('/addcat', async (req,res)=>{
+router.post('/addcat', async(req, res) => {
     const { cat_nam } = req.body;
     const category = { cat_nam };
-    await pool.query('INSERT INTO categories SET ?', [category], (err, category)=>{
-        if(err){
+    await pool.query('INSERT INTO categories SET ?', [category], (err, category) => {
+        if (err) {
             res.status(500).send('Error 500');
         }
-        if(!category){
+        if (!category) {
             res.status(400).send('Error 400');
         }
-        res.status(200).send({category});
+        res.status(200).send(category);
     });
 });
 
-router.post('/editcat/:id', async (req,res)=>{
+router.post('/editcat/:id', async(req, res) => {
     const { id } = req.params;
     const { cat_nam } = req.body;
     const category = { cat_nam };
-    await pool.query('UPDATE categories SET ? WHERE cat_id = ?', [category, id], (err, category)=>{
-        if(err){
+    await pool.query('UPDATE categories SET ? WHERE cat_id = ?', [category, id], (err, category) => {
+        if (err) {
             res.status(500).send('Error 500');
         }
-        if(!category){
+        if (!category) {
             res.status(400).send('Error 400');
         }
-        res.status(200).send({category});
+        res.status(200).send(category);
     });
 });
 
-router.get('/delcat/:id', async (req,res)=>{
+router.get('/delcat/:id', async(req, res) => {
     const { id } = req.params;
-    await pool.query('DELETE FROM categories WHERE cat_id = ?',[id]);
+    await pool.query('DELETE FROM categories WHERE cat_id = ?', [id]);
     res.send('Category removed');
 });
 
 // brands
 
-router.get('/findbra/:id', async (req, res)=>{
+router.get('/findbra/:id', async(req, res) => {
     const { id } = req.params;
     const brand = await pool.query('SELECT * FROM brands WHERE bra_id = ?', [id]);
-    res.send({brand});
+    res.send(brand);
 });
 
-router.get('/findallbra', async (req,res)=>{
+router.get('/findallbra', async(req, res) => {
     const brands = await pool.query('SELECT * FROM brands');
-    res.send({brands});
+    res.send(brands);
 });
 
-router.post('/addbra', async (req,res)=>{
+router.post('/addbra', async(req, res) => {
     const { bra_nam } = req.body;
     const brand = { bra_nam };
-    await pool.query('INSERT INTO brands SET ?', [brand], (err, brand)=>{
-        if(err){
+    await pool.query('INSERT INTO brands SET ?', [brand], (err, brand) => {
+        if (err) {
             res.status(500).send('Error 500');
         }
-        if(!brand){
+        if (!brand) {
             res.status(400).send('Error 400');
         }
-        res.status(200).send({brand});
+        res.status(200).send(brand);
     });
 });
 
-router.post('/editbra/:id', async (req,res)=>{
+router.post('/editbra/:id', async(req, res) => {
     const { id } = req.params;
     const { bra_nam } = req.body;
     const brand = { bra_nam };
-    await pool.query('UPDATE brands SET ? WHERE bra_id = ?', [brand, id], (err, brand)=>{
-        if(err){
+    await pool.query('UPDATE brands SET ? WHERE bra_id = ?', [brand, id], (err, brand) => {
+        if (err) {
             res.status(500).send('Error 500');
         }
-        if(!brand){
+        if (!brand) {
             res.status(400).send('Error 400');
         }
-        res.status(200).send({brand});
+        res.status(200).send(brand);
     });
 });
 
-router.get('/delbra/:id', async (req,res)=>{
+router.get('/delbra/:id', async(req, res) => {
     const { id } = req.params;
-    await pool.query('DELETE FROM categories WHERE cat_id = ?',[id]);
+    await pool.query('DELETE FROM categories WHERE cat_id = ?', [id]);
     res.send('Brand removed');
 });
 
 // // products
 
-router.get('/findpro/:id', async (req, res)=>{
+router.get('/findpro/:id', async(req, res) => {
     const { id } = req.params;
     const product = await pool.query('SELECT * FROM products WHERE pro_id = ?', [id]);
-    res.send({product});
+    res.send(product);
 });
 
-router.get('/findallpro', async (req,res)=>{
+router.get('/findallpro', async(req, res) => {
     const products = await pool.query('SELECT * FROM products');
-    res.send({products});
+    res.send(products);
 });
 
-router.get('/fprobycat/:cat_id', async (req,res)=>{
+router.get('/fprobycat/:cat_id', async(req, res) => {
     const { cat_id } = req.params
     const products = await pool.query('SELECT * FROM products WHERE cat_id = ?', [cat_id]);
-    res.send({products});
+    res.send(products);
 });
 
-router.get('/fprobybra/:bra_id', async (req,res)=>{
+router.get('/fprobybra/:bra_id', async(req, res) => {
     const { bra_id } = req.params
     const products = await pool.query('SELECT * FROM products WHERE bra_id = ?', [bra_id]);
-    res.send({products});
+    res.send(products);
 });
 
-router.post('/addpro', async (req,res)=>{
+router.post('/addpro', async(req, res) => {
     const { pro_nam, pro_des, pro_pri, pro_sto, pro_sta, cat_id, bra_id } = req.body;
     const product = { pro_nam, pro_des, pro_pri, pro_sto, pro_sta, cat_id, bra_id };
-    await pool.query('INSERT INTO products SET ?', [product], (err, product)=>{
-        if(err){
+    await pool.query('INSERT INTO products SET ?', [product], (err, product) => {
+        if (err) {
             res.status(500).send('Error 500');
         }
-        if(!product){
+        if (!product) {
             res.status(400).send('Error 400');
         }
-        res.status(200).send({product});
+        res.status(200).send(product);
     });
 });
 
-router.post('/editpro/:id', async (req,res)=>{
+router.post('/editpro/:id', async(req, res) => {
     const { id } = req.params;
     const { pro_nam, pro_des, pro_pri, pro_sto, pro_sta, cat_id, bra_id } = req.body;
     const product = { pro_nam, pro_des, pro_pri, pro_sto, pro_sta, cat_id, bra_id };
-    await pool.query('UPDATE products SET ? WHERE pro_id = ?', [product, id], (err, product)=>{
-        if(err){
+    await pool.query('UPDATE products SET ? WHERE pro_id = ?', [product, id], (err, product) => {
+        if (err) {
             res.status(500).send('Error 500');
         }
-        if(!product){
+        if (!product) {
             res.status(400).send('Error 400');
         }
-        res.status(200).send({product});
+        res.status(200).send({ product });
     });
 });
 
-router.get('/delpro/:id', async (req,res)=>{
+router.get('/delpro/:id', async(req, res) => {
     const { id } = req.params;
     await pool.query('DELETE FROM products WHERE pro_id = ?', [id]);
     res.send('Product removed');
@@ -162,50 +162,50 @@ router.get('/delpro/:id', async (req,res)=>{
 
 // images
 
-router.get('/findimg/:id', async (req, res)=>{
+router.get('/findimg/:id', async(req, res) => {
     const { id } = req.params;
     const image = await pool.query('SELECT * FROM images WHERE img_id = ?', [id]);
-    res.send({image})
+    res.send(image)
 });
 
-router.get('/fimgbypro/:pro_id', async (req,res)=>{
+router.get('/fimgbypro/:pro_id', async(req, res) => {
     const { pro_id } = req.params;
     const images = await pool.query('SELECT * FROM images WHERE pro_id = ?', [pro_id]);
-    res.send({images});
+    res.send(images);
 });
 
-router.post('/addimg', async (req,res)=>{
+router.post('/addimg', async(req, res) => {
     const { img_nam, img_url, pro_id } = req.body;
     const image = { img_nam, img_url, pro_id };
     // const image = req.body;
-    await pool.query('INSERT INTO images SET ?', [image], (err, image)=>{
-        if(err){
+    await pool.query('INSERT INTO images SET ?', [image], (err, image) => {
+        if (err) {
             res.status(500).send('Error 500');
         }
-        if(!image){
+        if (!image) {
             res.status(400).send('Error 400');
         }
-        res.status(200).send({image});
+        res.status(200).send(image);
     });
 });
 
-router.post('/editimg/:id', async (req,res)=>{
+router.post('/editimg/:id', async(req, res) => {
     const { id } = req.params;
     const { img_nam, img_url, pro_id } = req.body;
     const image = { img_nam, img_url, pro_id };
     // const image = req.body;
-    await pool.query('UPDATE images SET ? WHERE img_id = ?', [image, id], (err, image)=>{
-        if(err){
+    await pool.query('UPDATE images SET ? WHERE img_id = ?', [image, id], (err, image) => {
+        if (err) {
             res.status(500).send('Error 500');
         }
-        if(!image){
+        if (!image) {
             res.status(400).send('Error 400');
         }
-        res.status(200).send({image});
+        res.status(200).send({ image });
     });
 });
 
-router.get('/delimg/:id', async (req,res)=>{
+router.get('/delimg/:id', async(req, res) => {
     const { id } = req.params;
     await pool.query('DELETE FROM images WHERE img_id = ?', [id]);
     res.send('Image removed');
