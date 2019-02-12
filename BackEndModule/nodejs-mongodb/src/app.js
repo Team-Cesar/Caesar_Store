bddb4effca411525a43ffd6027c802a465490d8a
EliezerQ <elie.qc95@gmail.com>
2019-02-12 15:09:42 -0500

    Protegiendo rutas con auth

--- BackEndModule/nodejs-mongodb/src/app.js
+++ BackEndModule/nodejs-mongodb/src/app.js
@@ -37,11 +37,12 @@ app.use(bodyParser.json());
 
 // Conexion a base de datos mongoDb
 // mongoose.connect('mongodb+srv://caesar_store:qFkKUQsB4uCxK9wy@cluster0-9ay4b.azure.mongodb.net/caesar_store?retryWrites=true')
-mongoose.connect('mongodb://localhost:27017/CesarStore')
-// mongoose.connect('mongodb://caesar:caesar5@ds129670.mlab.com:29670/db-caesar-store')
+// mongoose.connect('mongodb://localhost:27017/CesarStore')
+mongoose.connect('mongodb://caesar:caesar5@ds129670.mlab.com:29670/db-caesar-store')
     .then(db => console.log('MongoDb on. Enjoy :3'))
     .catch(err => console.log(err));
 
+    
 // middlewares
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
--- BackEndModule/nodejs-mongodb/src/controllers/User.js
+++ BackEndModule/nodejs-mongodb/src/controllers/User.js
@@ -1,5 +1,7 @@
 const passport = require('passport');
 
+var Usuario = require('../models/Users/user');
+
 var controller = {
     signin: async (req, res) => {
         passport.authenticate('local', (err, user, info) => {
@@ -21,6 +23,65 @@ var controller = {
                 res.status(401).json(info);
             }
         })(req, res);
+    },
+    signup: async(req,res) => {
+        let { user_username, user_name, user_lastname, user_email, user_pass } = req.body;
+        let user = new Usuario({
+            user_username,
+            user_name,
+            user_lastname,
+            user_email,
+            user_role: 3,
+            user_status: 'Activo',
+            purchase_list: []
+        });
+        user.setPassword(user_pass);
+
+        Usuario.findOne({user_username}, (req, usuario) => {
+            if (usuario == null){
+                var usuario = new Usuario();
+                usuario = user;
+            } else {
+                var usuario = usuario;
+                console.log(usuario);
+            }
+            usuario.save((err, usuario) => {
+                if (err) {
+                    return res.status(500).send({ Error: "Error 500 saving user"});
+                }
+                if (!usuario) {
+                    return res.status(404).send({ Error: "Error 404 saving user" });
+                }
+                var token = user.generateJwt();
+                return res.status(200).send(usuario);
+            });
+        });
+    },
+    getuser: async (req, res) => {
+        let { user_username } = req.params;
+        Usuario.findOne({user_username}, (req, usuario) => {
+            if (usuario == null){
+                return res.status(500).send({ Error: "Crear primero un usuario" });
+            } else {
+                return res.status(200).send(usuario);
+            }
+        });
+    },
+    deleteuser: async (req, res) => {
+        let { user_username } = req.body;
+        Usuario.findOneAndDelete({user_username},(err,usuario)=>{
+            if(err) return res.status(404).send({Error: 'Error 404: Usuario no encontrado'});
+            else if(!usuario) return res.status(500).send({Error: 'Error 500: Error en la busqueda de usuario'});
+            usuario.save((err, usuario) => {
+                if (err) {
+                    return res.status(500).send({ Error: "Error 500 deleting user" });
+                }
+                if (!usuario) {
+                    return res.status(404).send({ Error: "Error 404 deleting user"})
+                }
+                return res.status(200).send(usuario);
+            });
+        });
     }
 };
 
